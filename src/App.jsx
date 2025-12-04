da3b777ba9b3d9452820eec65f4c061717756d30
Ebba-Ene <eneqvist.ebba@gmail.com>
2025-12-04 16:13:36 +0100

    added router and linked to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import "./App.css"
+import Home from "./pages/Home/Home"
+import Todo from "./pages/Todo/Todo"
+import EventPlanner from "./pages/EventPlanner/EventPlanner"
+import Habits from "./pages/Habits/Habits"
 
 function App() {
   return (
     <>
+    <BrowserRouter>
       <h1>Startsida</h1>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/todo' element={<Todo/>}/>
+        <Route path='/eventplanner' element={<EventPlanner/>}/>
+        <Route path='/habits' element={<Habits/>}/>
+      </Routes>
+    </BrowserRouter>
+
     </>
   )
 }
