cb27a583d597964e752d693c1404fc5f47b93f23
Desirée Strand <desireestrxnd@gmail.com>
2025-12-08 15:01:47 +0100

    expand utils file to avoid repeating code

--- src/components/EventForm.jsx
+++ src/components/EventForm.jsx
@@ -1,7 +1,7 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { EventContext } from "../context/EventContext";
+import { formatDateTimeLocal, useSyncEndWithStart, validateEvent } from "./eventUtils";
 import EventInputs from "./EventInputs";
-import { formatDateTimeLocal } from "./date";
 
 const EventForm = () => {
     const { addEvent } = useContext(EventContext)
@@ -12,26 +12,12 @@ const EventForm = () => {
     const [end, setEnd] = useState(formatDateTimeLocal(now));
     const [name, setName] = useState("");
 
-    useEffect(() => {
-        if (new Date(end) < new Date(start)) {
-            setEnd(start);
-        }
-    }, [start]);
+    useSyncEndWithStart(start, end, setEnd);
 
     const handleAdd = () => {
-        if (name === "") {
-            alert("Eventet måste ha ett namn.");
-            return;
-        }
-        
-        if (new Date(start) < now) {
-            alert("Du kan inte välja ett förflutet datum.");
-            return;
-        }
-
-        if (new Date(end) < new Date(start)) {
-            alert("Sluttiden kan inte vara före starttiden.");
-            return;
+        const error = validateEvent(start, end, name)
+        if (error) {
+            return alert(error);
         }
         
         addEvent(start, end, name);
@@ -42,7 +28,7 @@ const EventForm = () => {
         <>
             <h3>Add Event</h3>
 
-            <EventInputs start={start} end={end} name={name} setStart={setStart} setEnd={setEnd} setName={setName}/>
+            <EventInputs start={start} end={end} name={name} setStart={setStart} setEnd={setEnd} setName={setName} minStart={formatDateTimeLocal(now)}/>
 
             <button onClick={handleAdd}>Add</button>
         </>
