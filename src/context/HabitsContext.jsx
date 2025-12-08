94f15110248e4e3995ad32eee7f026cfdebc6dda
codedcollections <ma.unander@gmail.com>
2025-12-08 11:43:12 +0100

    Add functionality useContext to send array to habits component and in component show array items and add CSS to display habits

--- src/context/HabitsContext.jsx
+++ src/context/HabitsContext.jsx
@@ -4,7 +4,9 @@ export const HabitsContext = createContext()
 
 const HabitsProvider = ({ children }) => {
   const [habits, setHabits] = useState([
-    { title: "read", repetitions: 2, priority: "low" },
+    { title: "läsa", repetitions: 2, priority: "låg" },
+    { title: "träna", repetitions: 1, priority: "medel" },
+    { title: "plugga", repetitions: 5, priority: "hög" },
   ])
   const showArray = (habits) => {
     console.log("showArray function sees habits as: " + JSON.stringify(habits))
--- src/pages/Habits/Habits.jsx
+++ src/pages/Habits/Habits.jsx
@@ -1,12 +1,25 @@
 import { useContext } from "react"
 import { HabitsContext } from "../../context/HabitsContext"
+import s from "./Habits.module.css"
 
 const Habits = () => {
   const { habits, showArray } = useContext(HabitsContext)
   return (
     <>
-      <h1>Habits component</h1>
-      {showArray(habits)}
+      <h1>Habits:</h1>
+      <div className={s.grid}>
+        {habits.map((item, i) => (
+          <div className={s.habitCard} key={i}>
+            <h2>{item.title}</h2>
+            <p>
+              <strong>repetitioner:</strong> {item.repetitions}
+            </p>
+            <p>
+              <strong>prioritet:</strong> {item.priority}{" "}
+            </p>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
