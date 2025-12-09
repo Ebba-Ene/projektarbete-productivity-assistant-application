c3b7b50ccd152084fa024687f398a7a87a770907
Ebba-Ene <eneqvist.ebba@gmail.com>
2025-12-09 11:29:35 +0100

    added function complete todo

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,19 +1,34 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { TodoContext } from "../../context/TodoContext"
+
+import todoCss from "./TodoList.module.css"
 
 const TodoList = () => {
 
-  const {todos} = useContext(TodoContext)
+  const {todos, completeTodo} = useContext(TodoContext)
+  const [isChecked, setIsChecked] = useState(todos)
+
+  console.log(todos)
 
   return(
-      <ul>
+      <ul className={todoCss.ul}>
         {todos.map((todo, id) => 
-        <li key={id}>
+        <li key={id} className={todo.status ? todoCss.complete : todoCss.notComplete}>
           <h3>{todo.title}</h3>
-          <p>{todo.description}</p>
-          <p>{todo.status ? "Klar" : "Inte klar"}</p>
-          <p>{todo.timeEstimate}</p>
           <p>{todo.category}</p>
+          <p>{todo.description}</p>
+
+          <form>
+            <label htmlFor="done">{todo.status ? "Klar" : "Inte klar"}</label>
+            {!todo.status &&
+              <input type="checkbox" id="done" checked={false} onChange={() => {completeTodo(id), console.log(todo.status)}}/>
+            }
+            {todo.status &&
+              <input type="checkbox" id="done" checked={true} onChange={() => {completeTodo(id), console.log(todo.status)}}/>
+            }
+          </form>
+          
+          <p>{todo.timeEstimateNumber} {todo.timeEstimate}</p>
           <p>{todo.deadline}</p>
         </li>)} 
       </ul>
