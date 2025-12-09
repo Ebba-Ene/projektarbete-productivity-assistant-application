3548619f4e7a06cfa1ae9f6724e6dd4983d84c78
Ebba-Ene <eneqvist.ebba@gmail.com>
2025-12-09 11:46:39 +0100

    fixed better timeEstimate

--- src/components/TodoForm.jsx/TodoForm.jsx
+++ src/components/TodoForm.jsx/TodoForm.jsx
@@ -1,26 +1,30 @@
 import { useContext, useState } from "react"
 import { TodoContext } from "../../context/TodoContext"
 
+import todoCss from "./TodoForm.module.css"
 
 const TodoForm = () => {
 
-  const {addTodo, show, setShow} = useContext(TodoContext)
+  const {addTodo, setShow} = useContext(TodoContext)
 
   const[title, setTitle] = useState("")
   const[description, setDescription] = useState("")
   const[timeEstimate, setTimeEstimate] = useState("")
+  const[timeEstimateNumber, setTimeEstimateNumber] = useState(0)
+
   const[category, setCategory] = useState("")
   const[deadline, setDeadline] = useState("")
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    if(category !== "Kategori"){
+    if(category){
       let newTodo = {
         title,
         description,
         status: false,
         timeEstimate,
+        timeEstimateNumber,
         category,
         deadline
       }
@@ -34,18 +38,25 @@ const TodoForm = () => {
   }
 
   return(
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className={todoCss.form}>
       <input type="text" placeholder="Titel" onChange={(e) => {setTitle(e.target.value)}}/>
       <input type="text" placeholder="Beskrivning" onChange={(e) => {setDescription(e.target.value)}}/>
-      <input type="text" placeholder="Tidsestimat" onChange={(e) => {setTimeEstimate(e.target.value)}}/>
+      <input type="number" placeholder="Tidsestimat?" onChange={(e) => {setTimeEstimateNumber(e.target.value)}}/>
+      <select value={timeEstimate} onChange={(e) => {setTimeEstimate(e.target.value)}}>
+        <option value="" disabled>Tidsform</option>
+        <option>minuter</option>
+        <option>timmar</option>
+        <option>dagar</option>
+      </select>
       <select value={category} onChange={(e) => {setCategory(e.target.value)}}>
-        <option>Kategori</option>
+        <option value="" disabled>Kategori</option>
         <option>Hälsa</option>
         <option>Hushåll</option>
         <option>Jobbrelaterat</option>
         <option>Nöje</option>
       </select>
-      <input type="text" placeholder="Deadline" onChange={(e) => {setDeadline(e.target.value)}}/>
+      {/* <input type="text" placeholder="Deadline" onChange={(e) => {setDeadline(e.target.value)}}/> */}
+      <input type="datetime-local"/>
 
       <button type="submit"> <strong>Lägg till ny todo</strong> {title}</button>
     </form>
