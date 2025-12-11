9196dcf58cf75d1ea03919360a320ca8dc78f9de
Ebba-Ene <eneqvist.ebba@gmail.com>
2025-12-11 13:17:48 +0100

    added random id, fixed button for time unit

--- src/components/TodoForm.jsx/TodoForm.jsx
+++ src/components/TodoForm.jsx/TodoForm.jsx
@@ -5,9 +5,9 @@ import todoCss from "./TodoForm.module.css"
 
 const TodoForm = () => {
 
-  const {addTodo, setShow} = useContext(TodoContext)
+  const {addTodo, setShow, todos} = useContext(TodoContext)
   const now = new Date()
-
+  
   const[title, setTitle] = useState("")
   const[description, setDescription] = useState("")
   const[timeEstimate, setTimeEstimate] = useState("")
@@ -17,35 +17,39 @@ const TodoForm = () => {
   
   const[timeEstimateUnit, setTimeEstimateUnit] = useState("")
   const[timeEstimateNumber, setTimeEstimateNumber] = useState(0)
+
   
-    useEffect(() => {
-      const newTimeEstimate = `${timeEstimateNumber} ${timeEstimateUnit}`
-      setTimeEstimate(newTimeEstimate)
-    }, [timeEstimateNumber, timeEstimateUnit])
-
+  useEffect(() => {
+    const newTimeEstimate = `${timeEstimateNumber} ${timeEstimateUnit}`
+    setTimeEstimate(newTimeEstimate)
+  }, [timeEstimateNumber, timeEstimateUnit])
+  
   const formatDateTimeLocal = () => {
-  const d = new Date()
-  const pad = (v) => v.toString().padStart(2, "0")
-  const year = d.getFullYear()
-  const month = pad(d.getMonth() + 1)
-  const day = pad(d.getDate())
-  return `${year}-${month}-${day}`
-};
-
+    const d = new Date()
+    const pad = (v) => v.toString().padStart(2, "0")
+    const year = d.getFullYear()
+    const month = pad(d.getMonth() + 1)
+    const day = pad(d.getDate())
+    return `${year}-${month}-${day}`
+  }
+  
   const handleSubmit = (e) => {
     e.preventDefault()
-
-    if(category && title && timeEstimate && Number(timeEstimateNumber) !== 0 && deadline && description){
+    
+    if(category && title && timeEstimate && timeEstimateNumber !== 0 && deadline && description){
       let newTodo = {
         title,
         description,
         status: false,
         timeEstimate,
         category,
-        deadline
-    }
-    addTodo(newTodo)
-    setShow(false)
+        deadline,
+        id: crypto.randomUUID()
+      }
+      
+      addTodo(newTodo)
+      setShow(false)
+      
     } else{alert("Fyll i alla tomma fält")}
   }
 
@@ -53,14 +57,25 @@ const TodoForm = () => {
     <form onSubmit={handleSubmit} className={todoCss.form}>
       <input type="text" placeholder="Titel" onChange={(e) => {setTitle(e.target.value)}}/>
       <input type="text" placeholder="Beskrivning" onChange={(e) => {setDescription(e.target.value)}}/>
-      <input type="number" placeholder="Tidsestimat?" onChange={(e) => {setTimeEstimateNumber(Number(e.target.value))}}/>
       
-      <select value={timeEstimateUnit} onChange={(e) => {setTimeEstimateUnit(e.target.value)}}>
-        <option value="" disabled>Tidsform</option>
-        <option>minuter</option>
-        <option>timmar</option>
-        <option>dagar</option>
-      </select>
+      <input type="number" placeholder="Tidsestimat?" onChange={(e) => {setTimeEstimateNumber(Number(e.target.value))}}/>
+        {timeEstimateNumber !== 1 && 
+          <select value={timeEstimateUnit} onChange={(e) => {setTimeEstimateUnit(e.target.value)}}>
+            <option value="" disabled>Tidsform</option>
+            <option>minuter</option>
+            <option>timmar</option>
+            <option>dagar</option>
+          </select>
+        }
+
+        {timeEstimateNumber == 1 && 
+        <select value={timeEstimateUnit} onChange={(e) => {setTimeEstimateUnit(e.target.value)}}>
+          <option value="" disabled>Tidsform</option>
+          <option>minut</option>
+          <option>timme</option>
+          <option>dag</option>
+        </select>
+        }
 
       <select value={category} onChange={(e) => {setCategory(e.target.value)}}>
         <option value="" disabled>Kategori</option>
