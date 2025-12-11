eb3857d65c7e34ffdee0aad00a4e26b4ee9d2b18
Ebba-Ene <eneqvist.ebba@gmail.com>
2025-12-11 15:18:56 +0100

    added function sortTodo

--- src/components/TodoSort/TodoSort.jsx
+++ src/components/TodoSort/TodoSort.jsx
@@ -5,9 +5,10 @@ const TodoSort = () => {
 
   const { sortTodo } = useContext(TodoContext)
   const [sort, setSort] = useState("")
-  const [direction, setDirection] = useState("Stigande")
+  const [direction, setDirection] = useState("")
  
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
     sortTodo(sort, direction)
   }
 
@@ -20,10 +21,29 @@ const TodoSort = () => {
         <option>Status</option>
       </select>
 
+    {sort === "Deadline" && 
       <select value={direction} onChange={(e) => {setDirection(e.target.value)}}>
-        <option>Stigande</option>
-        <option>Fallande</option>
-      </select>
+        <option value="" disabled>Välj</option>
+        <option value="Fallande">Närmast</option>
+        <option value="Stigande">Längs bort</option>
+      </select>    
+    }
+
+    {sort === "Tidsestimat" && 
+      <select value={direction} selected onChange={(e) => {setDirection(e.target.value)}}>
+        <option value="" disabled>Välj</option>
+        <option value="Fallande">Kortast</option>
+        <option value="Stigande">Längst</option>
+      </select>    
+    }
+
+    {sort === "Status" && 
+      <select value={direction} onChange={(e) => {setDirection(e.target.value)}}>
+        <option value="" disabled>Välj</option>
+        <option value="Fallande">Inte gjort</option>
+        <option value="Stigande">Gjort</option>
+      </select>    
+    }
 
       <button type="submit">Sortera</button>
     </form>
--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -4,40 +4,54 @@ export const TodoContext = createContext()
 
 const TodoProvider = ({children}) => {
   
-  const[show, setShow] = useState(false)
+  const [show, setShow] = useState(false)
+  const [showFilterBtn, setShowFilterBtn] = useState(false)
 
   const [todos, setTodos] = useState(JSON.parse(localStorage.getItem("todos")) || [])
 
-useEffect(() => {
-  localStorage.setItem("todos", JSON.stringify(todos))
-}, [todos])
+  const[filter, setFilter] = useState("")
+  const[whatToFilter, setWhatToFilter] = useState("")
+
+
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
 
   const addTodo = (todo) => {
     setTodos([...todos, todo])
   }
 
-  const removeTodo = (index) => {
-    const remainingTodos = todos.filter((_, i) => i !== index)
+  const removeTodo = (id) => {
+
+    const remainingTodos = todos.filter(todo => todo.id !== id)
     setTodos(remainingTodos)
 }
 
-  const completeTodo = (index) => {
+  const completeTodo = (todoId) => {
 
-    let newTodoList = [...todos]
-
-    if(newTodoList[index].status == false){
-      newTodoList[index].status = true
-    }else{
-      newTodoList[index].status = false
-    }
+    const foundObject = todos.filter((todo) => todo.id === todoId)
+    const placement = todos.indexOf(foundObject[0])
+    const newTodoList = [...todos]
     
+    if (newTodoList[placement].status === false) {
+
+      newTodoList[placement].status = true
+
+      
+    } else if (newTodoList[placement].status === true) {
+
+      newTodoList[placement].status = false
+    } 
+
     setTodos(newTodoList)
   }
 
   const timeToMinutes = (time) => {
-    const units = {minuter: 1, timmar: 60, dagar: 1440}
+    const units = {minut: 1, minuter: 1, timme: 60, timmar: 60, dag: 1440, dagar: 1440}
 
-    const parts = time.match(/^(\d+)\s(minuter|timmar|dagar)$/)
+    const parts = time.match(/^(\d+)\s(minut|minuter|timme|timmar|dag|dagar)$/)
 
     if(!parts)
       throw new Error(`'${time}' - Unexpected time format`)
@@ -48,35 +62,43 @@ useEffect(() => {
   }
 
   const sortTodo = (sorting, direction) => {
-    if(sorting == "Status"){
-      if(direction == "Fallande"){
-        const sorted = [...todos].sort((a,b) => a.status - b.status)
-        setTodos(sorted)
-      } else {
-      const sorted = [...todos].sort((a,b) => b.status - a.status)
-        setTodos(sorted)
-      }
-    } else if(sorting == "Deadline"){
+    
+      if(sorting == "Status"){
         if(direction == "Fallande"){
-          const sorted = [...todos].sort((a, b) => new Date(a.deadline) - new Date(b.deadline))
+          const sorted = [...todos].sort((a,b) => a.status - b.status)
           setTodos(sorted)
-        } else {
-          const sorted = [...todos].sort((a, b) => new Date(b.deadline) - new Date(a.deadline))
+        } else if(direction == "Stigande"){
+        const sorted = [...todos].sort((a,b) => b.status - a.status)
           setTodos(sorted)
-      }
-    } else if (sorting == "Tidsestimat"){
-        if(direction == "Fallande"){
-          const sorted = [...todos].sort((a, b) => timeToMinutes(a.timeEstimate) - timeToMinutes(b.timeEstimate))
-          setTodos(sorted)
-        }else{
-          const sorted = [...todos].sort((a, b) => timeToMinutes(b.timeEstimate) - timeToMinutes(a.timeEstimate))
-          setTodos(sorted)
+        }
+      } else if(sorting == "Deadline"){
+          if(direction == "Fallande"){
+            const sorted = [...todos].sort((a, b) => new Date(a.deadline) - new Date(b.deadline))
+            setTodos(sorted)
+          } else if(direction == "Stigande"){
+            const sorted = [...todos].sort((a, b) => new Date(b.deadline) - new Date(a.deadline))
+            setTodos(sorted)
+          }
+      }  else if (sorting == "Tidsestimat"){
+          if(direction == "Fallande"){
+            const sorted = [...todos].sort((a, b) => timeToMinutes(a.timeEstimate) - timeToMinutes(b.timeEstimate))
+            setTodos(sorted)
+          }else if(direction == "Stigande"){
+            const sorted = [...todos].sort((a, b) => timeToMinutes(b.timeEstimate) - timeToMinutes(a.timeEstimate))
+            setTodos(sorted)
+        
       }
     }
   }
+
+  const filterTodo = (filter, what) => {
+    setFilter(filter)
+    setWhatToFilter(what)
+    setShowFilterBtn(true)
+  }
   
   return(
-    <TodoContext value={{todos, addTodo, show, setShow, completeTodo, removeTodo, sortTodo}}>
+    <TodoContext value={{todos, addTodo, show, setShow, completeTodo, removeTodo, sortTodo, filterTodo, filter, whatToFilter, showFilterBtn, setShowFilterBtn, setFilter}}>
       {children}
     </TodoContext>
   )
