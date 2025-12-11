a83592e39a22f54c7cc3fbc903c142d8e09cbe27
Desirée Strand <desireestrxnd@gmail.com>
2025-12-11 10:44:32 +0100

    add link to event planner on homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,31 @@
+import { useContext } from "react"
+import { EventContext } from "../../context/EventContext"
+import { formatDisplayDate } from "../../components/helper"
+import { Link } from "react-router-dom"
+
 const HomePage = () => {
+const { upcomingEvents } = useContext(EventContext)
 
+  return (
+    <>
+      <h1>Livsplanerare</h1>
+      <h2>Startsida</h2>
 
-  return(
-    <h1>Startsida</h1>
+      <h3>Ej utförda ärenden</h3>
+
+      <h3>Kommande händelser</h3>
+      <ul>
+        {upcomingEvents.slice(0,3).map((event, index) => 
+          <li key={index}>
+            {formatDisplayDate(event.start)} – {formatDisplayDate(event.end)}:{" "}
+            <strong>{event.name}</strong>
+          </li>
+        )}
+      </ul>
+      <Link to='/eventplanner'>Alla händelser</Link>
+
+      <h3>Rutiner</h3>
+    </>
   )
 }
 
