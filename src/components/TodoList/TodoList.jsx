c1e518c0cbcad5d51dcc18e137adf5893be05dd9
Ebba-Ene <eneqvist.ebba@gmail.com>
2025-12-15 14:21:12 +0100

    showing different todo lists depending on logged in user

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -5,10 +5,12 @@ import todoCss from "./TodoList.module.css"
 import TodoListItem from "../TodoListItem/TodoListItem"
 import TodoSort from "../TodoSort/TodoSort"
 import TodoFilter from "../TodoFilter/TodoFilter"
+import { UserContext } from "../../context/UserContext"
 
 const TodoList = () => {
 
   const {todos, filter, whatToFilter, sort, howToSort} = useContext(TodoContext)
+  const {currentUser} = useContext(UserContext)
 
   const timeToMinutes = (time) => {
     const units = {minut: 1, minuter: 1, timme: 60, timmar: 60, dag: 1440, dagar: 1440}
@@ -17,14 +19,16 @@ const TodoList = () => {
 
     if(!parts)
       throw new Error(`'${time}' - Unexpected time format`)
-    const scalar = parseInt(parts[1])
-    const minutes = units[parts[2]]
+      const scalar = parseInt(parts[1])
+      const minutes = units[parts[2]]
 
     return minutes*scalar
-  }
+    }
 
-  
-  const filterTodos = todos.filter(todo => {
+
+    const userTodos = todos.filter(todo => todo.userId === currentUser.userId)
+
+    const filterTodos = userTodos.filter(todo => {
     if (filter === "Status" && whatToFilter === "Utförd") {
       return todo.status === true
     }
@@ -37,22 +41,53 @@ const TodoList = () => {
     return true
   })
   
-  const sortedTodos = [...filterTodos].sort((a, b) => {
-    if (sort === "Status") {
-      return howToSort === "Fallande" ? a.status - b.status : b.status - a.status
-    }
-    if (sort === "Deadline") {
-      return howToSort === "Fallande"
-      ? new Date(a.deadline) - new Date(b.deadline)
-      : new Date(b.deadline) - new Date(a.deadline)
-    }
-    if (sort === "Tidsestimat") {
-      return howToSort === "Fallande"
-      ? timeToMinutes(`${a.timeEstimateNumber} ${a.timeEstimateUnit}`) - timeToMinutes(`${b.timeEstimateNumber} ${b.timeEstimateUnit}`)
-      : timeToMinutes(`${b.timeEstimateNumber} ${b.timeEstimateUnit}`) - timeToMinutes(`${a.timeEstimateNumber} ${a.timeEstimateUnit}`)
-    }
-    return 0
-  })
+    const sortedTodos = [...filterTodos].sort((a, b) => {
+      if (sort === "Status") {
+        return howToSort === "Fallande" ? a.status - b.status : b.status - a.status
+      }
+      if (sort === "Deadline") {
+        return howToSort === "Fallande"
+        ? new Date(a.deadline) - new Date(b.deadline)
+        : new Date(b.deadline) - new Date(a.deadline)
+      }
+      if (sort === "Tidsestimat") {
+        return howToSort === "Fallande"
+        ? timeToMinutes(`${a.timeEstimateNumber} ${a.timeEstimateUnit}`) - timeToMinutes(`${b.timeEstimateNumber} ${b.timeEstimateUnit}`)
+        : timeToMinutes(`${b.timeEstimateNumber} ${b.timeEstimateUnit}`) - timeToMinutes(`${a.timeEstimateNumber} ${a.timeEstimateUnit}`)
+      }
+      return 0
+    })
+  
+
+  // const filterTodos = todos.filter(todo => {
+  //   if (filter === "Status" && whatToFilter === "Utförd") {
+  //     return todo.status === true
+  //   }
+  //   if (filter === "Status" && whatToFilter === "Ej utförd") {
+  //     return todo.status === false
+  //   }
+  //   if (filter === "Kategori") {
+  //     return todo.category === whatToFilter
+  //   }
+  //   return true
+  // })
+  
+  // const sortedTodos = [...filterTodos].sort((a, b) => {
+  //   if (sort === "Status") {
+  //     return howToSort === "Fallande" ? a.status - b.status : b.status - a.status
+  //   }
+  //   if (sort === "Deadline") {
+  //     return howToSort === "Fallande"
+  //     ? new Date(a.deadline) - new Date(b.deadline)
+  //     : new Date(b.deadline) - new Date(a.deadline)
+  //   }
+  //   if (sort === "Tidsestimat") {
+  //     return howToSort === "Fallande"
+  //     ? timeToMinutes(`${a.timeEstimateNumber} ${a.timeEstimateUnit}`) - timeToMinutes(`${b.timeEstimateNumber} ${b.timeEstimateUnit}`)
+  //     : timeToMinutes(`${b.timeEstimateNumber} ${b.timeEstimateUnit}`) - timeToMinutes(`${a.timeEstimateNumber} ${a.timeEstimateUnit}`)
+  //   }
+  //   return 0
+  // })
 
   return(
     <div>
